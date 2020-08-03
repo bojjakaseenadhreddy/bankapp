09b7cf04b272e4cfdc8d276f4aa560ce165cf0f0
kaseenadh bojja <kaseenadh111@gmail.com>
2020-08-03 16:59:14 +0530

    impoerted HttpClientModule in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -8,7 +9,8 @@ import { SharedRoutingModule } from './shared-routing.module';
   declarations: [],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    HttpClientModule
   ]
 })
 export class SharedModule { }
